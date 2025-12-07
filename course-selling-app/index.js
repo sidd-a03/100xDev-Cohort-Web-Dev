9b518088c1df85bbdf043236f8d164544227a5c5
Achinta <achintasiddhanta0304@gmail.com>
2025-12-08 02:40:27 +0530

    added admin router

--- course-selling-app/index.js
+++ course-selling-app/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRouter from "./routes/user.router.js";
 import courseRouter from "./routes/course.router.js";
+import adminRouter from "./routes/admin.router.js";
 
 const app = express();
 const PORT = 3000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use('/api/v1/user', userRouter);
 
 app.use('/api/v1/course', courseRouter);
+
+app.use('/api/v1/admin', adminRouter)
 
 app.listen(PORT, () => {
     console.log(`Subscription tracker is running on http://localhost:${PORT}`);
