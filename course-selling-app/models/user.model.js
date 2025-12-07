469f313b2c815b96e82ff4e81d6c06a13b9f2dee
Achinta <achintasiddhanta0304@gmail.com>
2025-12-08 04:52:03 +0530

    modify the schema model

--- course-selling-app/models/user.model.js
+++ course-selling-app/models/user.model.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose'
 
-const Schema = mongoose.Schema;
-
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
