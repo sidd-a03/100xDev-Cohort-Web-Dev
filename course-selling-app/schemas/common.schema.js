764891aa0a706ebcf6aaaa4c825bb84de02a9872
Achinta <achintasiddhanta0304@gmail.com>
2025-12-08 18:36:30 +0530

    modified the zod schema

--- course-selling-app/schemas/common.schema.js
+++ course-selling-app/schemas/common.schema.js
@@ -4,10 +4,10 @@ export const signUpSchema = z.object({
     username: z.union([
         z.email(),
         z.string().regex(
-            /^[A-Za-z._-]+$/,
+            /^[a-z._-]+$/,
             "Username can only contain alphabets, '.', '-', '_'"
-        ).trim().toLowerCase()
-    ]),
+        )
+    ]).transform(value => value.trim().toLowerCase()),
     password: z.string(),
     name: z.string().min(3).max(100)
 });
