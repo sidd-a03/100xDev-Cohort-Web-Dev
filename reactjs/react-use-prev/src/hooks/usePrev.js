7af6bb734f14b0e1518fe61435a062fab8bdf78f
Achinta <achintasiddhanta0304@gmail.com>
2025-12-13 17:49:59 +0530

    clear view

--- reactjs/react-use-prev/src/hooks/usePrev.js
+++ reactjs/react-use-prev/src/hooks/usePrev.js
@@ -2,9 +2,13 @@ import {useEffect, useRef} from "react";
 
 const usePrev = (count) => {
     const ref = useRef(null);
+    console.log(`From inside usePrev hook. the count is ${count} and ref.current is ${ref.current}`);
     useEffect(() => {
+        console.log(`From inside useEffect before initialize ref.current is ${ref.current}`);
         ref.current = count;
+        console.log(`From inside useEffect after initialize ref.current is ${ref.current}`);
     }, [count]);
+    console.log(`Outside useEffect after initialize ref.current is ${ref.current}`);
     return ref;
 }
 
